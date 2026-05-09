789f233aafb93d27e6eebe25ccda44a4c82ba5ce
Vladislav <vladishka200312@gmail.com>
2026-05-09 14:55:33 +0300

    mongoDb

--- server.js
+++ server.js
@@ -1,24 +1,60 @@
 const express = require('express');
-const fs = require('fs');
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb://admin1:12346@ac-kkox6wi-shard-00-00.dxuoa90.mongodb.net:27017,ac-kkox6wi-shard-00-01.dxuoa90.mongodb.net:27017,ac-kkox6wi-shard-00-02.dxuoa90.mongodb.net:27017/?ssl=true&replicaSet=atlas-ysxoti-shard-0&authSource=admin&appName=Cluster0')
+.then(() => console.log('MongoDB подключена'))
+.catch(err => console.log(err));
 
 app.use(express.json());
 app.use(express.static('public'));
 
 const PASSWORD = "1234";
 
-app.get('/api/schedule', (req, res) => {
-    const data = fs.readFileSync('schedule.json');
-    res.json(JSON.parse(data));
+const ScheduleSchema = new mongoose.Schema({
+    data: Object
 });
 
-app.post('/api/schedule', (req, res) => {
+const Schedule = mongoose.model('Schedule', ScheduleSchema);
+
+app.get('/api/schedule', async (req, res) => {
+
+    let schedule = await Schedule.findOne();
+
+    if (!schedule) {
+        schedule = await Schedule.create({
+            data: {}
+        });
+    }
+
+    res.json(schedule.data);
+});
+
+app.post('/api/schedule', async (req, res) => {
+
     if (req.headers.authorization !== PASSWORD) {
         return res.status(403).send("Нет доступа");
     }
 
-    fs.writeFileSync('schedule.json', JSON.stringify(req.body, null, 2));
+    let schedule = await Schedule.findOne();
+
+    if (!schedule) {
+
+        schedule = new Schedule({
+            data: req.body
+        });
+
+    } else {
+
+        schedule.data = req.body;
+
+    }
+
+    await schedule.save();
+
     res.send("OK");
 });
 
 app.listen(3000, () => console.log("http://localhost:3000"));
+
+
